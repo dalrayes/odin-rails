732d44c233d9ffe8ea9d16cbb011b30e66b6a597
Dalal Alrayes <dalrayes@gmail.com>
2015-04-13 23:12:44 -0500

    form based workflow feature completed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,8 @@ end
 def create
 	@article = Article.new(article_params)
 	@article.save
+
+	flash.notice = "Article '#{@article.title}' Created!"
 	redirect_to article_path(@article)
 end
 
@@ -34,6 +36,8 @@ end
 def destroy
 	@article = Article.find(params[:id])
 	@article.destroy
+
+	flash.notice = "Article '#{@article.title}' Deleted"
 	redirect_to articles_path
 end
 
